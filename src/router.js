6e57dd3ca7d12a7f5718fcda773be55179e07cdb
Caleb Hart <caleb_hart11@yahoo.com>
2023-01-21 13:02:30 -0800

    forgot an s on on the index path loader lol

--- src/router.js
+++ src/router.js
@@ -15,7 +15,7 @@ import Show from './pages/Show';
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path="/" element={<App />}>
-			<Route path="" element={<Index />} loader={gigLoader} />
+			<Route path="" element={<Index />} loader={gigsLoader} />
 			<Route path=":id" element={<Show />} loader={gigLoader} />
 			<Route path="create" action={createAction}/>
 			<Route path="update/:id" action={updateAction}/>
