ed8a86d7af3eac401bd717c11dcc5157ac1ed9fd
Rawle Becerril <rawlesxm@gmail.com>
2023-01-25 21:35:34 -0500

    map and autocomplete components, index.html script added for places API

---
+++ src/components/map.js
@@ -0,0 +1,40 @@
+import {useJsApiLoader, GoogleMap, Marker} from "@react-google-maps/api"
+
+
+
+const center={ lat: 48.8584, lng: 2.2945}
+
+const apiKey = "AIzaSyDU1_Xmnfu1L_dBvs1MICHBrcXM84nOpv4"
+
+
+
+
+export default function Map(props){
+
+    const searchInput=useRef(null)
+
+    const {isLoaded} = useJsApiLoader({
+        googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY
+    })
+
+    if(!isLoaded){
+        return <h1>loading...</h1>
+    }
+
+     
+    return (<>
+
+
+   <div style={{backgroundColor: "grey", height: "400px", width: "400px", marginLeft: "auto", marginRight: "auto"}} className="map">
+            <GoogleMap center={center} zoom={15} mapContainerStyle={{height: '100%',  width: '100%'}}>
+                <Marker position={center}/>
+                <Marker position={center}/>
+
+            </GoogleMap>
+
+
+    </div>
+
+    
+    </>)
+}
